d91c5e4e531fdc32d561bd7cda643060bdb033c9
Amir <amir.a.23@hotmail.com>
2021-11-04 22:18:53 +0000

    refactored and tested using moch chai

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,37 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
-console.log(args);
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body); // Print the HTML for the Google homepage.
-  //console.log(typeof(body));
-  if (body === null) {
-    console.log("The breed is not found"); //check if breed is found
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-  }
+
+const fetchBreedDescription = function(breedName, callback) {
+  //onsole.log(breedName);
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    //console.log('error:', error); // Print the error if one occurred
+    //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //console.log('body:', body); // Print the HTML for the Google homepage.
+    //console.log(typeof(body));
+
+  
+    //const data = JSON.parse(body);
+    //console.log('data:', data.length);
+    let results = null;
+    let breedError = null;
+    if(error) {
+      breedError = error;
+    } else {
+      const data = JSON.parse(body);
+      if(data.length > 0) {
+        results = data[0].description;
+      } else {
+        breedError = 'empty';
+      }
+    }
+    
+  
+    callback(breedError, results);
+
+
   //console.log(typeof data);
+  });
 
-});
+};
+
+module.exports = { fetchBreedDescription };
